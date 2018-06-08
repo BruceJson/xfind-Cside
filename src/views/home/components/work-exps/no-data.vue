db1284787a911ad45043779613f37673bbd11ec9
zhaodan <1376020043@qq.com>
2018-06-08 17:37:24 +0800

    all

---
+++ src/views/home/components/work-exps/no-data.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class='nodata_box'>
+        <div class='title'>
+            <p>求职意向</p>
+            <img class='edit_btn' src="@imgs/home/9.png" alt="" @click='editClick'>
+        </div>
+        <p class='nodata_tip'>完善求职意向，我们将为您更精准的推荐工作！</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'workexps-no-data',
+    props: {
+        detail: {
+            type: Object,
+            default () {
+                return {};
+            }
+        }
+    },
+    methods: {
+        editClick() {
+            this.$emit('edit');
+        }
+    }
+}
+
+</script>
+<style>
+@import '@pages/home/edit-resume.css';
+
+</style>
